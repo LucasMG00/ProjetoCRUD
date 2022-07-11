28aefc85dd4856f9988d6dfdb1894958316a7c78
Lucas MG <lu.mang.gon@gmail.com>
2022-07-11 19:10:14 -0300

    lombok update

--- src/br/com/produto/model/Produto.java
+++ src/br/com/produto/model/Produto.java
@@ -1,5 +1,8 @@
 package br.com.produto.model;
 
+import lombok.*;
+
+@Getter @Setter
 public class Produto {
 
     private String id;
@@ -7,35 +10,4 @@ public class Produto {
     private String valor;
     private String quantidade;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setValor(String valor) {
-        this.valor = valor;
-    }
-
-    public void setQuantidade(String quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getValor() {
-        return valor;
-    }
-
-    public String getQuantidade() {
-        return quantidade;
-    }
 }
